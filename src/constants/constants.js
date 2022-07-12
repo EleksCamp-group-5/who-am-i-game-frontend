e23328e9a75c01eaecdaca7486a2611db50ad047
Yurii <yurii.lypchanskyi@gmail.com>
2022-07-12 12:22:00 +0300

    Changed value status

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 const WAITING_FOR_PLAYERS =
-  'com.eleks.academy.whoami.core.state.WaitingForPlayers';
+  'WAITING_FOR_PLAYERS';
 const SUGGESTING_CHARACTERS =
-  'com.eleks.academy.whoami.core.state.SuggestingCharacters';
+  'SUGGEST_CHARACTER';
 const PROCESSING_QUESTION =
-  'com.eleks.academy.whoami.core.state.ProcessingQuestion';
+  'GAME_IN_PROGRESS';
 const NUMBER_OF_PLAYERS = 4;
 const READY = 'READY';
 const NOT_READY = 'NOT_READY';
